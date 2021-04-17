58bea7e18fc4ca837547b0f03648f3d6c3f97a71
pardeep2690 <bigbenprojects@gmail.com>
2021-04-17 21:18:02 +0530

    added jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,18 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+
+$(function() {
+    alert("hello world");
+});
+
+
+
